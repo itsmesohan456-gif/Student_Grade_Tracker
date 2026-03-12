cec4324aa8698440ac12d13373ccd58f786bb7c5
Sohan Shrestha <itsmesohan456@gmail.com>
2026-03-12 20:25:07 +0545

    Added CSV file to save those students' grade records

--- Student_Grade_Tracker_Project/studentgradetracker.py
+++ Student_Grade_Tracker_Project/studentgradetracker.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 def calculate_average(grades):
     if not grades:
         return 0
@@ -18,40 +22,54 @@ def main():
 
     
         if choice == '1':
-            name = input("\nEnter the student's name:")
+            name1 = input("\nEnter the student's name:")
 
-            subject = input("Enter subject of the student {name}:")
+            subject = input("Enter subject of the student:")
 
             try:
-                grade = float(input(f"Enter marrks for the subject {subject}:"))
+                grade = float(input(f"Enter marks for the subject {subject}:"))
 
                 student_data = {
-                "name": name,
+                "name": name1,
                 "subject": subject,
                 "grade": grade
                 }
                 students.append(student_data)
 
-                print(f"Data for the student {name} saved successfully!")
+                print(f"Data for the student {name1} saved successfully!")
 
             except ValueError:
                 print("Invalid input. Please enter the valid marks for the given {subject}.")
 
         elif choice == '2':
+            filename = "student_grade_records.csv"
+            fields = ['ID', 'Name', 'Subject', 'Grade']
+
             if not students:
                 print("\nNo data stored yet. Please add a student record first.")
             else:
-                print("\n--- Current Grade Report ---")
+                try:
+                    with open(filename, mode = 'w', newline='') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(fields)
+                        print("\n--- Current Grade Report ---")
 
-                all_grades = []
-                for s in students:
-                    print(f"Student: {s['name']}  | Subject: {s['subject']}  | Grade: {s['grade']}")
+                        all_grades = []
+                        for s in students:
+                            print(f"Student: {s['name']}  | Subject: {s['subject']}  | Grade: {s['grade']}")
+
+                            writer.writerow([s.get('id', 'N/A'), s['name'], s['subject'], s['grade']])
             
-                all_grades.append(s['grade'])
+                        all_grades.append(s['grade'])
 
-                avg = calculate_average(all_grades)
-                print(f"\nTotal Students: {len(students)}")
-                print(f"Overall Class Average: {avg:.2f}")
+                        avg = calculate_average(all_grades)
+                        print(f"\nTotal Students: {len(students)}")
+                        print(f"Overall Class Average: {avg:.2f}")
+
+                    print(f"\nSucessfully saved the record to {filename}")
+                except Exception as e:
+                    print(f"Error in trying to save the record in CSV: {e}")
+        
         elif choice == '3':
             print("Exiting Program.....\nGoodBye!")
             break
